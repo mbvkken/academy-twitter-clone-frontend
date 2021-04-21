d10a564f770feccb3e0f222475924fb68d4c068d
Mathias Bakken <mbakken0@gmail.com>
2021-04-21 15:49:06 +0200

    cra on heroku

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+// import Authenticate from './components/Authenticate'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+    <Switch>
+      <Route path="/" component={Login} />
+    </Switch>
+    </HashRouter>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { checkUser } from '../services/session';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginDetails: {
+                handle: '',
+                password: ''
+            },
+            error: null,
+            loginStatus: false,
+
+        }
+    }
+
+    handleInputChange(field, event) {
+        this.setState({
+            loginDetails: {
+                ...this.state.loginDetails,
+                [field]: event.target.value
+            }
+        });
+    }
+
+    async handleLoginClick(event) {
+        event.preventDefault();
+        const { history } = this.props;
+        const { handle, password } = this.state.loginDetails;
+
+        try {
+            this.setState({ loginStatus: true, error: null });
+            const { token, error } = await checkUser({ handle, password }); 
+
+            if (error) {
+                throw new Error(error);
+            }
+            if (!token) {
+                throw new Error('no token- try again');
+            }
+
+            localStorage.setItem('twitter_clone_token', token);
+            history.push('/');
+            }
+             catch (error) {
+            this.setState({ error, loginStatus: false })
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Login</h1>
+                <form> 
+                    <label>
+                        Handle:
+                        <input 
+                        type="text"
+                        value={this.state.loginStatus.handle}
+                        onChange={this.handleInputChange.bind(this, 'handle')}
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input 
+                        type="password"
+                        value={this.state.loginStatus.password}
+                        onChange={this.handleInputChange.bind(this, 'password')}
+                        />
+                    </label>
+                    <div>
+                       <button onClick={this.handleLoginClick.bind(this)}>Login</button> 
+                    </div>
+                </form>                    
+            </div>
+        )
+    }
+}
+
+export default Login;
+
